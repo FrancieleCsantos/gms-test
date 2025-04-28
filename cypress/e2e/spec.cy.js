6fd1aa3d1c95424bbd91fc85563b35dc04845a3c
FrancieleCSantos <francielecoceicaodossantos93@gmail.com>
2025-04-28 20:07:36 -0300

    meu primeiro commit

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress"/>
+
+describe('US-012-Funcionalidade: Cadastro de membros', () => {
+  it('Cadastro de campos orbigatórios', () => {
+    cy.visit('http://127.0.0.1:8080/')
+    cy.get('#signup-firstname').type("Franciele")
+    cy.get('#signup-lastname').type("Conceição")
+    cy.get('#signup-email').type("Franciele2@gmail.com")
+    cy.get('#signup-phone').type("71983791733")
+    cy.get('#signup-password').type("Pipoca@10")
+    cy.get('#signup-button').click()
+    cy.get('#signup-response').should("contain", "Cadastro realizado com sucesso!" )
+
+
+  
+  })
+})
